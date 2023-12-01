d198418bb977906b48ded39ffd1a424385e6bc59
Albert <albertzeap@gmail.com>
2023-11-30 17:36:23 -0800

    Add faulty code to test linter

---
+++ index.js
@@ -0,0 +1,19 @@
+// This code intentionally violates ESLint rules
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  console.log("Sum:", add(1, 2));
+  
+  // Variables declared but never used
+  const unusedVariable = "I'm not used";
+  
+  // Missing semicolon at the end of the line
+  const missingSemicolon = "I should have a semicolon"
+  
+  // Space before function parentheses
+  const example = function () {
+    console.log("I violate the space rule");
+  };
+  
